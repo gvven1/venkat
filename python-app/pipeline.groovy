0d5b9150659c943a2e199636fb1f23eca016f2c5
Venkat <gvven1@gmail.com>
2024-12-14 17:50:43 +0530

    update user

--- python-app/pipeline.groovy
+++ python-app/pipeline.groovy
@@ -13,7 +13,7 @@
     steps {
     script {
     withCredentials([usernamePassword(credentialsId: 'my-docker-hub-credential', usernameVariable: 'DOCKER_USERNAME', passwordVariable: 'DOCKER_PASSWORD')]) {
-    sh "docker login -u amuldevopstools -p DRz6lLLVh2hbpArHpBZ3cnETJupZ7o1zppn14wyEwf+ACRAMWeOu amuldevopstools.azurecr.io"
+    sh "docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD amuldevopstools.azurecr.io"
     sh "docker build -t amuldevopstools.azurecr.io/python_jenkins:v2 ./python-app"
     sh "docker push amuldevopstools.azurecr.io/python_jenkins:v2"
     }
